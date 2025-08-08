f60bce0399237d63fb53f80eb172ab4ce72b9cca
Kevyn <kevyn.silvano@escola.pr.gov.br>
2025-08-08 12:11:52 -0300

    API

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(express.json())
+const usuarios = []
+app.post('/usuarios', (req, res)=>{
+    const usuario = req.body
+    usuarios.push(usuario)
+    res.send("usuario cadratrado com sucesso")
+})
+app.get('/usuarios', (req, res)=>{
+    res.send(usuarios)
+})
 app.get('/ola', (req, res)=>{
     res.send("ola 3°ds!")
 })
